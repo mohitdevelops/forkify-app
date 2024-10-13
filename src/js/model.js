76036bee93eac94e321d30d78dea920260240706
Mohit <mohit@rohininfo.com>
2024-10-13 19:03:19 +0530

    created helpers and split the code

--- src/js/model.js
+++ src/js/model.js
@@ -1,23 +1,25 @@
+import { API_URL } from './config';
+import { getJSON } from './helpers';
+
 export const state = {
   recipe: {},
 };
 
 export const loadRecipe = async recipeId => {
-  const response = await fetch(
-    `https://forkify-api.herokuapp.com/api/v2/recipes/${recipeId}`
-  );
-  const data = await response.json();
-  if (!response.ok) throw new Error(`${data.message} (${response.status})`);
-  let { recipe } = data.data;
-  state.recipe = {
-    id: recipe.id,
-    cookingTime: recipe.cooking_time,
-    image: recipe.image_url,
-    ingredients: recipe.ingredients,
-    publisher: recipe.publisher,
-    servings: recipe.servings,
-    sourceUrl: recipe.source_url,
-    title: recipe.title,
-  };
-  console.log(state.recipe);
+  try {
+    const data = await getJSON(`${API_URL}/${recipeId}`);
+    let { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      cookingTime: recipe.cooking_time,
+      image: recipe.image_url,
+      ingredients: recipe.ingredients,
+      publisher: recipe.publisher,
+      servings: recipe.servings,
+      sourceUrl: recipe.source_url,
+      title: recipe.title,
+    };
+  } catch (err) {
+    console.log(`${err} ⚠`);
+  }
 };
